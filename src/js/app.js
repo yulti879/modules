d6c2c715aa96eef3159209cdd5c937a08789a5b0
yulti879 <162827622+yulti879@users.noreply.github.com>
2024-12-09 00:03:29 +0300

    добавлен пробел перед закрывающей скобкой

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,4 @@
-import Game, { GameSavingData, readGameSaving as loadGame, writeGameSaving as saveGame} from './game.js';
+import Game, { GameSavingData, readGameSaving as loadGame, writeGameSaving as saveGame } from './game.js';
 
 const game = new Game();
 
