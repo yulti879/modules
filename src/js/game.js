8b65693aa965c93c4f7cc0660592ec014b0dfe06
yulti879 <162827622+yulti879@users.noreply.github.com>
2024-12-09 10:31:07 +0300

    отредактирован синтаксис

--- src/js/game.js
+++ src/js/game.js
@@ -4,13 +4,13 @@ export default class Game {
     start() {
       console.log('game started');
     }
-  };
+  }
   
   export class GameSavingData {
-  };
+  }
   
   export function readGameSaving() {
-  };
+  }
   
   export function writeGameSaving() {
-  };
+  }
